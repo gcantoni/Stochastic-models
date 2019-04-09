f5859aa5984e0c0095757c55d387b1d065b87c14
Giorgio Cantoni <giorgio.canto98@gmail.com>
2019-04-09 13:29:21 +0200

    Update Gaussian_Mixture.m

--- AlgoritmoEM/Gaussian_Mixture.m
+++ AlgoritmoEM/Gaussian_Mixture.m
@@ -7,12 +7,13 @@ MU2 = [-1 .5];
 SIGMA2 = [1 0; 0 1];
 X = [mvnrnd(MU1,SIGMA1,1000);mvnrnd(MU2,SIGMA2,500)];
 
+
 scatter(X(:,1),X(:,2),10,'.')
 hold on
 
 disp_opt = 'iter'; %'final'
 options = statset('Display',disp_opt);
-k = 2;
+k = 2; % k = 3 , k = 5
 obj = fitgmdist(X,k,'Options',options);
 obj2 = gmdistribution.fit(X,k,'Options',options);
 h = ezcontour(@(x,y)pdf(obj,[x y]),[-8 6],[-8 6]);
